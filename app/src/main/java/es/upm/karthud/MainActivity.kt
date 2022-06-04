49b302809f5020b9a6fdeee8d32c7ce29d7a9868
VicDominguez <viconel27@gmail.com>
2022-06-04 17:39:06 +0200

    Añadida gestión de permisos y refactorizacion

--- app/src/main/java/es/upm/karthud/MainActivity.kt
+++ app/src/main/java/es/upm/karthud/MainActivity.kt
@@ -1,118 +1,145 @@
+/**
+ * @author: Victor Manuel Dominguez Rivas y Juan Luis Moreno Sancho
+ */
+
 package es.upm.karthud
 
-import android.location.Location
-import android.location.LocationListener
 import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import kotlin.math.roundToInt
 
-class MainActivity : AppCompatActivity(), LocationListener
+class MainActivity : AppCompatActivity(), LocationListener, EasyPermissions.PermissionCallbacks
 {
-    private lateinit var speedText: TextView
-    private lateinit var latText: TextView
-    private lateinit var longText: TextView
+    //gestion de permisos
+    private val gpsPermission: String = Manifest.permission.ACCESS_FINE_LOCATION
+    private val gpsPermissionRequest : Int = 123
 
-    private lateinit var locationManager: LocationManager
+    //constantes
+    private val minTimeMs: Long = 250
+    private val minDistance : Float = 0.0f
+    private val msToKmh : Double = 3.6
 
-    //estados de la actividad
+    //textviews
+    private val speedText: TextView by lazy { findViewById(R.id.speedText) }
+    private val latText: TextView by lazy { findViewById(R.id.latText) }
+    private val longText: TextView by lazy { findViewById(R.id.longText) }
+
+    //objetos manager
+    private val locationManager : LocationManager by lazy { getSystemService(Context.LOCATION_SERVICE) as LocationManager }
+
+    private var speed: Float = 0.0f
+
+    /*
+    ---------------------------------------------------------
+        Estados de la actividad
+    ---------------------------------------------------------
+     */
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        speedText = findViewById(R.id.speedText)
-        latText = findViewById(R.id.latText)
-        longText = findViewById(R.id.longText)
-
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        checkAppPermissions()
-        executeHUD()
-
+        checkAndStartGPS()
     }
-
 
     override fun onPause()
     {
         super.onPause()
-        locationManager.removeUpdates(this)
+        stopGPS()
     }
 
     override fun onResume()
     {
         super.onResume()
-        executeHUD()
+        checkAndStartGPS()
     }
 
     override fun onDestroy()
     {
         super.onDestroy()
+        stopGPS()
+    }
+
+    /*
+    ---------------------------------------------------------
+        Callbacks de manejo de permisos
+    ---------------------------------------------------------
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>,
+                                            grantResults: IntArray)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>)
+    {
+        Toast.makeText(this, "Permisos concedidos, iniciando GPS", Toast.LENGTH_SHORT).show()
+        startGPSUnchecked()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>)
+    {
+        speedText.text = getString(R.string.kmh,"?")
+        latText.text = getString(R.string.lat, "?")
+        longText.text = getString(R.string.longitude, "?")
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms))
+            AppSettingsDialog.Builder(this).build().show()
+    }
+
+    /*
+    ---------------------------------------------------------
+        Gestión de la funcionalidad
+    ---------------------------------------------------------
+     */
+
+    /**
+     * Comprueba si la app dispone de permisos. Si los dispone arranca el GPS y si no solicita los permisos.
+     * @see startGPSUnchecked
+     */
+    private fun checkAndStartGPS()
+    {
+        if (EasyPermissions.hasPermissions(this, gpsPermission))
+           startGPSUnchecked()
+        else
+            EasyPermissions.requestPermissions(this, getString(R.string.gps_not_granted),gpsPermissionRequest, gpsPermission)
+    }
+
+    /**
+     * Arranca la solicitud de localizaciones sin comprobar si dispone de permisos o no.
+     * @see checkAndStartGPS
+     */
+    @SuppressLint("MissingPermission")
+    private fun startGPSUnchecked()
+    {
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTimeMs, minDistance, this)
+    }
+
+    private fun stopGPS()
+    {
         locationManager.removeUpdates(this)
     }
 
-    private fun checkAppPermissions()
+    /*
+    ---------------------------------------------------------
+        Callback de LocationListener
+    ---------------------------------------------------------
+     */
+    override fun onLocationChanged(l: Location)
     {
-        //This if is for SDK >=23, but our target is SDK 28
-        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-            checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
-            requestPermissions(Array<String>(1){Manifest.permission.ACCESS_FINE_LOCATION},1000)
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1000)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                executeHUD()
-            else
-                finish()
-        }
-    }
-
-
-    private fun executeHUD()
-    {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 250,0.0f, this)
-        Toast.makeText(this, "Waiting for GPS connection", Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onLocationChanged(l: Location) {
-        val speed = l.speed * 3.6f
+        speed = ((l.speed * msToKmh * 10.0).roundToInt() / 10.0).toFloat()
         speedText.text = getString(R.string.kmh,speed.toString())
         latText.text = getString(R.string.lat, l.latitude.toString())
         longText.text = getString(R.string.longitude, l.longitude.toString())
-        Log.d("MainActivity", "Time: " + l.time)
-        Log.d("MainActivity", "Raw speed: " +l.speed)
     }
-
 
 }
